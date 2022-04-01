c3f7213c340ba6e0d1c535e00d4e925401b0fc39
kukymber <ostap_to@mail.ru>
2022-04-01 22:00:35 +0800

    redid fill_fb

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -6,7 +6,7 @@ from mainapp.models import ProductCategory, Product
 
 
 def load_from_json(file_name):
-    with open(file_name, mode='r', encoding='utf-8') as infile:
+    with open(file_name, mode='r', encoding='UTF-16') as infile:
         return json.load(infile)
 
 
@@ -15,16 +15,16 @@ class Command(BaseCommand):
         User.objects.create_superuser(username='Anatolii', email='mail@mail.ru', password='1')
         category = load_from_json('C:/Users/ElenaOstapenko/PycharmProjects/django/django_homework/mainapp/fixtures/category.json')
 
-        ProductCategory.objects.all().delite()
+        ProductCategory.objects.all().delete()
         for categor in category:
             cat = categor.get('fields')
             cat['id'] = categor.get('pk')
             new_category = ProductCategory(**cat)
             new_category.save()
 
-        products = load_from_json('C:/Users/ElenaOstapenko/PycharmProjects/django/django_homework/mainapp/fixtures/product.json')
+        products = load_from_json('C:/Users/ElenaOstapenko/PycharmProjects/django/django_homework/mainapp/fixtures/products.json')
 
-        Product.objects.all().delite()
+        Product.objects.all().delete()
         for product in products:
             prod = product.get('fields')
             category = prod.get('category')
